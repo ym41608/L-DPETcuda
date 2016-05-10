bb439c6630ca38a066d85d7cfd5af7c3db43e99c
ym41608 <hytseng@media.ee.ntu.edu.tw>
2016-05-10 14:10:38 +0000

    ver6

--- device_common.h
+++ device_common.h
@@ -1,7 +1,7 @@
 #ifndef DEVICE_COMMON_H
 #define DEVICE_COMMON_H
 
-static const int BLOCK_SIZE = 192;
+static const int BLOCK_SIZE = 256;
 static const int SAMPLE_NUM = 444;
 static const int SAMPLE_NUMT = 512;
 
